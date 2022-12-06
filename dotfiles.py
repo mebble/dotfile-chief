bf92a036781674fca6bfd4b399e189ca71e55682
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2022-12-06 12:49:44 +0530

    Refactor the script and test the sample

--- dotfiles.py
+++ dotfiles.py
@@ -4,12 +4,6 @@ import shutil
 import sys
 from pathlib import Path
 
-home_dir = os.environ['HOME']
-commands = ['onboard', 'release']
-script_path = os.path.dirname(__file__)
-dotfiles_json_path = os.path.join(script_path, './dotfiles.json')
-dotfiles_dir_path = os.path.join(script_path, './dotfiles')
-
 def touch(path):
     Path(os.path.dirname(path)).mkdir(parents=True, exist_ok=True)
     Path(path).touch()
@@ -37,6 +31,17 @@ def get_cmd():
         return ''
     return cmd
 
+def get_operation():
+    command_map = {
+        'onboard': onboard,
+        'release': release,
+    }
+
+    cmd = get_cmd()
+    operation = command_map.get(cmd)
+
+    return operation
+
 def validate(dotfiles):
     src_set = { file['src'] for file in dotfiles }
     dest_set = { file['dest'] for file in dotfiles }
@@ -45,32 +50,33 @@ def validate(dotfiles):
     if len(dest_set) != len(dotfiles):
         return 'There are duplicate "dest" fields'
 
-cmd = get_cmd()
-if cmd not in commands:
-    print('Usage: python3 dotfiles.py (onboard | release)')
-    exit(1)
-
-if cmd == 'onboard':
-    operate = onboard
-elif cmd == 'release':
-    operate = release
-
-with open(dotfiles_json_path) as df:
-    dotfiles = json.load(df)
-
-    err = validate(dotfiles)
-    if err:
-        print(f'[FAIL] dotfiles.json has a problem: {err}')
+if __name__ == '__main__':
+    operate = get_operation()
+    if operate is None:
+        print('Usage: python3 dotfiles.py (onboard | release)')
         exit(1)
 
-    for file in dotfiles:
-        name = file['name']
+    home_dir = os.environ['HOME']
+    script_path = os.path.dirname(__file__)
+    dotfiles_json_path = os.path.join(script_path, './dotfiles.json')
+    dotfiles_dir_path = os.path.join(script_path, './dotfiles')
 
-        if file.get('ignore', False):
-            print(f'[IGNORE] dotfile {name} is ignored')
-            continue
+    with open(dotfiles_json_path) as df:
+        dotfiles = json.load(df)
 
-        src = file['src'].replace('~', home_dir)
-        dest = os.path.join(dotfiles_dir_path, file['dest'])
+        err = validate(dotfiles)
+        if err:
+            print(f'[FAIL] dotfiles.json has a problem: {err}')
+            exit(1)
 
-        operate(name, src, dest)
+        for file in dotfiles:
+            name = file['name']
+
+            if file.get('ignore', False):
+                print(f'[IGNORE] dotfile {name} is ignored')
+                continue
+
+            src = file['src'].replace('~', home_dir)
+            dest = os.path.join(dotfiles_dir_path, file['dest'])
+
+            operate(name, src, dest)
